469b0c1ac8fef59831b6cbdc3512f35883eecb78
Sarfraz Ahmed Shar <86257930+Sarfraz-Baloch@users.noreply.github.com>
2022-01-06 14:24:08 +0500

    Create Spro.py

---
+++ Spro.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+ 
+import os,sys,random,struct
+ 
+x = str(struct.calcsize("P") * 8)
+
+if '64' in x:
+ 
+    os.system('chmod 777 s64 && ./s64')
+ 
+else:
+ 
+    print('\033[1;31m   aarch cannot identified\033[0;97m')
+    os.system('xdg-open https://youtube.com/channel/UCg5PqZRoQx6ZhuH5JBmgSFA')
+    os.sys.exit()
+ 
+ 
